ae5c83d4d26f867824797723a0dc0c01428b6f4c
Guillermo Quintana <guille121093@gmail.com>
2024-07-12 19:27:25 -0300

    nextJS front

--- frontend/kairos/tailwind.config.ts
+++ frontend/kairos/tailwind.config.ts
@@ -1,50 +1,20 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-	darkMode: 'selector',
-	theme: {
-		fontFamily: {
-			'sans': ['"IBM Plex Sans"', 'ui-sans-serif', 'system-ui'],
-			'serif': ['"IBM Plex Serif"', 'ui-serif'],
-			'mono': ['ui-monospace', 'SFMono-Regular'],
-			'display': ['Oswald'],
-			'body': ['"IBM Plex Sans"'],
-		},
-		extend: {
-			borderRadius: {
-				lg: "2px",
-				md: "1.5px",
-				sm: "1px",
-			},
-			colors: {
-				// flowbite-svelte
-				primary: {
-					50: '#FFF5F2',
-					100: '#FFF1EE',
-					200: '#FFE4DE',
-					300: '#FFD5CC',
-					400: '#FFBCAD',
-					500: '#FE795D',
-					600: '#EF562F',
-					700: '#EB4F27',
-					800: '#CC4522',
-					900: '#A5371B'
-				},
-				sblack: {
-					50: '#0A0A0B',
-					100: '#0A0A0B',
-					200: '#0A0A0B',
-					300: '#0A0A0B',
-					400: '#0A0A0B',
-					500: '#0A0A0B',
-					600: '#0A0A0B',
-					700: '#0A0A0B',
-					800: '#0A0A0B',
-					900: '#0A0A0B'
-				},
-			}
-		}
-	},
-	plugins: [require('@tailwindcss/typography', 'flowbite/plugin')]
-} as Config;
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
